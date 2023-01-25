ed26ebafc5f5d95fd470930c4394713992fff619
molarmanful <benjinpang@gmail.com>
2023-01-25 01:00:07 -0500

    works?

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import BabScene from './components/BabScene.vue'
+</script>
+
+<template>
+  <BabScene></BabScene>
+</template>
---
+++ src/components/BabScene.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { createScene } from '../scenes/x0'
+
+const bcan = ref(null)
+
+onMounted(_ => {
+  if (bcan.value) createScene(bcan.value)
+})
+</script>
+
+<template>
+  <section flex justify-center items-center>
+    <canvas ref="bcan" w-screen h-screen outline-0></canvas>
+  </section>
+</template>
